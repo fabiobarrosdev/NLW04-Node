b9d748773eaaa2a0e062cc8f542aaeda36ee2dbd
fabio.barros <fabio.barros.dev@gmail.com>
2021-02-23 17:01:55 -0300

    Banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
+import 'reflect-metadata'
 import express from 'express';
+import "./database";
+import { router } from './routes';
 
 const app = express();
 
-// 1 param => Rota(Recurso API)
-// 2 param => request, response
-app.get("/", (request, response) =>{
-
-    return response.json({ kelela: "Hello NUB"});
-});
-
-app.post("/", (request, response) =>{
-    //Recebe os dads para cadastro
-    return response.json({ message: "Dados Salvos com sucesso NUB"});
-});
+app.use(express.json());
+app.use(router);
 
 
 app.listen(3333, () => console.log("Servidor rodando"));
